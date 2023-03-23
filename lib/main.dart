8ae8563619c8b18b67592604f7e62aaa71e3a543
Mukhammadaliti <mukhammadaliti02@gmail.com>
2023-03-23 13:01:15 +0600

    clock is done

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:clock_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
