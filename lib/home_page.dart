ad2826494ead1844dbc4797154e50182d1bd99fa
Mukhammadaliti <mukhammadaliti02@gmail.com>
2023-03-25 14:39:55 +0600

    dart time

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:clock_app/clock_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,71 +10,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var me = DateTime.now();
+  // var formattedTime = DateFormat('').format(me);
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        body: SafeArea(
-          child: Container(
-            alignment: Alignment.center,
-            color: Color(0xff2d2f41),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  "Clock",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-                Text(
-                  "Clock",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 64,
-                  ),
-                ),
-                Text(
-                  "Clock",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-                ClockView(),
-                Text(
-                  "Timezone",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.language,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "Timezone",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                      ),
-                    ),
-                    Text(
-                      "UTC",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+    return Scaffold(
+      backgroundColor: Color(0xff2d2f41),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        // alignment: Alignment.center,
+        // color: Color(0xff2d2f41),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Clock",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
             ),
-          ),
+            SizedBox(
+              height: 32,
+            ),
+            Text(
+              "Clock",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 64,
+              ),
+            ),
+            Text(
+              "Clock",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            ClockView(),
+            Text(
+              "Timezone",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.language,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "UTC",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
